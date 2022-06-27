76d2900f58aca8033ea2ad15b1df89a5373fdd07
EMNayon <emranmasudnayon.017@gmail.com>
2022-06-28 00:34:28 +0600

    added new files and solve the average pair prblm for array

---
+++ multi_pointer_28-06-2022.js
@@ -0,0 +1,17 @@
+// complexity O(logn)
+function averagePain(arr, num){
+    let l = 0
+    let r = arr.length - 1
+    console.log(r);
+    let count = 0
+    while(l < r){
+        count++
+        let avg = (arr[l] + arr[r]) / 2
+        if(avg === num) return true
+        else if(avg > num) r--
+        else l++
+        console.log(count);
+    }
+    return undefined
+}
+console.log(averagePain([-2,0,1,2,4,5,6,7,1,34,5,3,2,1,34,4],5.5))
